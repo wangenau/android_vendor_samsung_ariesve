arco/android_vendor_samsung_ancora

Add libI420colorconvert.so
 * Fixes preview issue in Movie Editor

device-vendor-blobs.mk
@@ -70,6 +70,7 @@ PRODUCT_COPY_FILES += \
 
 # Media libs
 PRODUCT_COPY_FILES += \
+    vendor/samsung/ancora/proprietary/lib/libI420colorconvert.so:system/lib/libI420colorconvert.so \
     vendor/samsung/ancora/proprietary/lib/libmm-color-convertor.so:system/lib/libmm-color-convertor.so \
     vendor/samsung/ancora/proprietary/lib/libDivxDrm.so:system/lib/libDivxDrm.so \
     vendor/samsung/ancora/proprietary/lib/libExtendedExtractor.so:system/lib/libExtendedExtractor.so \

wangenau/android_vendor_samsung_ariesve

device-vendor-blobs.mk
@@ -77,6 +77,7 @@ PRODUCT_COPY_FILES += \
 
 # Media libs
 PRODUCT_COPY_FILES += \
+    vendor/samsung/ariesve/proprietary/lib/libI420colorconvert.so:system/lib/libI420colorconvert.so \
     vendor/samsung/ariesve/proprietary/lib/libmm-color-convertor.so:system/lib/libmm-color-convertor.so \
     vendor/samsung/ariesve/proprietary/lib/libDivxDrm.so:system/lib/libDivxDrm.so \
     vendor/samsung/ariesve/proprietary/lib/libExtendedExtractor.so:system/lib/libExtendedExtractor.so \