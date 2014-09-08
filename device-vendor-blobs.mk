arco/android_vendor_samsung_ancora

Add OMX lib for hardware MP3 playback

device-vendor-blobs.mk
@@ -73,7 +73,8 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/ancora/proprietary/lib/libSHIMDivxDrm.so:system/lib/libSHIMDivxDrm.so \
     vendor/samsung/ancora/proprietary/lib/libmmipl.so:system/lib/libmmipl.so \
     vendor/samsung/ancora/proprietary/lib/libmmosal.so:system/lib/libmmosal.so \
-    vendor/samsung/ancora/proprietary/lib/libmmparser.so:system/lib/libmmparser.so
+    vendor/samsung/ancora/proprietary/lib/libmmparser.so:system/lib/libmmparser.so \
+    vendor/samsung/ancora/proprietary/lib/libOmxMp3Dec.so:system/lib/libOmxMp3Dec.so
 
 # Firmware
 PRODUCT_COPY_FILES += \

wangenau/android_vendor_samsung_ariesve

device-vendor-blobs.mk
@@ -74,7 +74,8 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/ariesve/proprietary/lib/libSHIMDivxDrm.so:system/lib/libSHIMDivxDrm.so \
     vendor/samsung/ariesve/proprietary/lib/libmmipl.so:system/lib/libmmipl.so \
     vendor/samsung/ariesve/proprietary/lib/libmmosal.so:system/lib/libmmosal.so \
-    vendor/samsung/ariesve/proprietary/lib/libmmparser.so:system/lib/libmmparser.so
+    vendor/samsung/ariesve/proprietary/lib/libmmparser.so:system/lib/libmmparser.so \
+    vendor/samsung/ariesve/proprietary/lib/libOmxMp3Dec.so:system/lib/libOmxMp3Dec.so
 
 # Firmware
 PRODUCT_COPY_FILES += \