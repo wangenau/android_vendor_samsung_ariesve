2edf4910d529ca904c48e3ee650f5c03a2530488
Arne Coucheron <arco68@gmail.com>
2014-10-11 11:29:15 +0200

    Fix compilation after rild updates
    * Don't really think we need to copy these to obj anymore.
      Can't remember why we did it in the first place anyway.

--- device-vendor-blobs.mk
+++ device-vendor-blobs.mk
@@ -17,9 +17,7 @@
 # Prebuilt libraries that are needed to build open-source libraries
 PRODUCT_COPY_FILES += \
     vendor/samsung/ariesve/proprietary/lib/libaudioalsa.so:obj/lib/libaudioalsa.so \
-    vendor/samsung/ariesve/proprietary/lib/libril.so:obj/lib/libril.so \
-    vendor/samsung/ariesve/proprietary/lib/libseccameraadaptor.so:obj/lib/libseccameraadaptor.so \
-    vendor/samsung/ariesve/proprietary/lib/libsecril-client.so:obj/lib/libsecril-client.so
+    vendor/samsung/ariesve/proprietary/lib/libseccameraadaptor.so:obj/lib/libseccameraadaptor.so
 
 # Binaries
 PRODUCT_COPY_FILES += \
